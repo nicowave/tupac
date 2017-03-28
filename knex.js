759b36d06470e94e6b6b6fc780e6369a005daed6
nicowave <geoidlife@gmail.com>
2017-03-28 11:13:32 -0600

    added migration file and knex configuration files

--- knex.js
+++ knex.js
@@ -3,13 +3,8 @@
 
 'use strict';
 
-module.exports = {
-  development:{
-    client: 'pg',
-    connection: 'postgres://localhost/blog_dev'
-  },
-  test: {
-    client: 'pg',
-    connection: 'postgres://localhost/blog_test'
-  }
-};
+const environment = process.env.NODE_ENV || 'development';
+const knexConfig = require('./knexfile')[environment];
+const knex = require('knex')(knexConfig);
+
+module.exports = knex;
--- knexfile.js
+++ knexfile.js
@@ -3,12 +3,16 @@
 
 'use strict';
 
-const environment = process.env.NODE_ENV || 'development';
-const knexConfig = require('./knexfile')[environment];
-const knex = require('knex')(knexConfig);
-
-module.exports = knex;
-
+module.exports = {
+  development:{
+    client: 'pg',
+    connection: 'postgres://localhost/blog_dev'
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/blog_test'
+  }
+};
 
 
 // $ knex migrate:make users
