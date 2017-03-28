f3694b82df20abde548dd117eb427c5b542ea81e
nicowave <geoidlife@gmail.com>
2017-03-28 11:55:30 -0600

    added index handlebars file javascript rendering of index and head.hbs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+
+
+//post route because form submission is a POST
+router.post('/', function(req, res, next){
+  var user = req.body.username;
+
+  if (user === ''){
+    res.render('index', {error: "Error"});
+  } else {
+    res.redirect(`/${user}`);
+  }
+})
 
 module.exports = router;
